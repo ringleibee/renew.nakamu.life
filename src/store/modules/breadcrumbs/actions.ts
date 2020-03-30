f8a74489f39247c5b03d886b4b514037fb073aec
福田 王璽 <fukuda-o1@almexinc.local>
2020-03-30 13:08:54 +0900

    all del

--- src/store/modules/breadcrumbs/actions.ts
+++
@@ -1,5 +0,0 @@
-import { SET_BREADCRUMBS } from './mutations'
-
-export const setBreadcrumbs = (breadcrumbs: any) => {
-  SET_BREADCRUMBS(breadcrumbs)
-}
--- src/store/modules/breadcrumbs/mutations.ts
+++
@@ -1,8 +0,0 @@
-import { reactive } from '@vue/composition-api'
-
-import state from './state'
-
-export const SET_BREADCRUMBS = (breadcrumbs = []) => {
-  const reactiveState = reactive(state)
-  reactiveState.breadcrumbs = breadcrumbs
-}
